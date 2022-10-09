cf2b5f677a5e23069284c93ac4a8218df3056501
Olga <Olga_kopylova9@mail.ru>
2022-10-09 22:40:46 +0500

    Задание4

---
+++ Homework2/EX4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход семь чисел, 
+// и находит их среднее арифметическое
+
+int [] array = {10, 20, 30, 4};
+int i = 0;
+int sum = 0;
+int n = array.Length;
+for (i=0; i<n; i++);
+sum (sum + array[i]);
+{
+    Console.WriteLine(sum);
+
+}
+
+

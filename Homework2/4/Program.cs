3b2efa3178b2d289c0e71cc25cb9bc39ef56dde4
Olga <Olga_kopylova9@mail.ru>
2022-10-09 23:11:38 +0500

    задание4

---
+++ Homework2/4/Program.cs
@@ -0,0 +1,19 @@
+﻿//сумма элементов массива
+
+int [] array = {1,2,3,4,5,6,7};
+int n = array.Length;
+int x = 0;
+int sum = 0;
+int rezult = 0;
+ 
+
+ for (int i = 1; i <= n; i++)
+ {
+    sum = +x;
+    rezult = sum / n;
+      }
+      {
+            Console.WriteLine(rezult);
+      }
+
+           
